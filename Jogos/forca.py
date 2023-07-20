719f429d57f4d6bb1570f2252942e825ab8df54c
Camila <camissk@hotmail.com>
2023-07-19 21:46:06 -0300

    Manipulando strings

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -8,10 +8,16 @@ def jogar():
     acertou = False
 
     while not enforcou and not acertou:
+        chute = input("Qual letra?").strip()
+        index = 0
+        for letra in palavra_secreta:
+            if chute.lower() == letra.lower():
+                print(f"Encontrei a letra {letra} na posição {index}")
+            index += 1
+
         print("jogando...")
 
     print("Fim do jogo!")
-
 
 if (__name__ == "__main__"):
     jogar()
