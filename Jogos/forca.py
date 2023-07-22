78840d0a8c11117ff2794a8ee5e2f56b77647d57
Camila <camissk@hotmail.com>
2023-07-22 14:08:37 -0300

    Deixando o jogo da forca mas dinâmico com leitura de arquivos

--- Jogos/forca.py
+++ Jogos/forca.py
@@ -1,23 +1,50 @@
+import random
 def jogar():
-    print("********************************")
+    print('********************************')
     print("Bem vindo ao jogo de Forca!")
     print("********************************")
 
-    palavra_secreta = "banana"
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0, len(palavras))
+    palavra_secreta = palavras[numero].upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while not enforcou and not acertou:
-        chute = input("Qual letra?").strip()
-        index = 0
-        for letra in palavra_secreta:
-            if chute.lower() == letra.lower():
-                print(f"Encontrei a letra {letra} na posição {index}")
-            index += 1
+        chute = input("Qual letra?").strip().upper()
 
-        print("jogando...")
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == len(palavra_secreta)
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou!")
+    else:
+        print("Você perdeu!")
 
     print("Fim do jogo!")
 
-if (__name__ == "__main__"):
+
+if __name__ == "__main__":
     jogar()
