3a61b581544eb58ce11e4c5c195d488d3880c606
Camila <camissk@hotmail.com>
2023-07-26 20:43:26 -0300

    Removendo duplicação com herança

---
+++ Filmes/modelo.py
@@ -0,0 +1,70 @@
+class Programa:
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    def dar_like(self):
+        self._likes += 1
+
+class Filme (Programa):
+    def __init__(self, nome, ano, duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    def dar_like(self):
+        self._likes += 1
+
+class Serie (Programa):
+    def __init__(self, nome, ano, temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
+
+    @property
+    def likes(self):
+        return self._likes
+
+    @property
+    def nome(self):
+        return self._nome
+
+    @nome.setter
+    def nome(self, novo_nome):
+        self._nome = novo_nome
+
+    def dar_like(self):
+        self._likes += 1
+
+
+vingadores = Filme("vingadores - guerra infinita", 2018, 160)
+vingadores.dar_like()
+vingadores.dar_like()
+print(f"Nome: {vingadores.nome} - Ano: {vingadores.ano} - Duração: {vingadores.duracao} - Likes: {vingadores.likes}")
+
+atlanta = Serie("atlanta", 2018, 2)
+atlanta.dar_like()
+print(f"Nome: {atlanta.nome} - Ano: {atlanta.ano} - Temporadas: {atlanta.temporadas} - Likes: {atlanta.likes}")
